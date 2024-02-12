e8dfb60afd43281bd02bb0e7658aab5af0d9e765
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-02-12 17:30:23 +0000

    Update DelayMech.c

--- HW4/DelayMech.c
+++ HW4/DelayMech.c
@@ -1,8 +1,9 @@
 #include "DelayMech.h"
 
 void delay_ms(unsigned long long ms) {
-    unsigned long cycles = ms * 16000; // Correct calculation for 16MHz CPU, assuming 1 millisecond = 16000 cycles
-
+    unsigned long long pre = ms * 160;///168;//00/168; //*(/1,68); // Correct calculation for 16MHz CPU, assuming 1 millisecond = 16000 cycles
+    unsigned long long scale =200*pre;
+    unsigned long long cycles=scale/500;
     // Loop that decrements 'cycles' until it reaches 0
     while(cycles > 0){
         __asm__ __volatile__("nop"); // Perform a no-operation to ensure the loop body is not empty
