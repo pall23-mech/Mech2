d4db50f180800ca4a936dcfcef1eab9c95460279
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-02-12 13:26:52 +0000

    Create DelayMech.h

---
+++ HW4/DelayMech.h
@@ -0,0 +1,14 @@
+/*
+ * DelayMech.h
+ *
+ *  Created on: Feb 12, 2024
+ *      Author: palli
+ */
+
+#ifndef DELAYMECH_H_
+#define DELAYMECH_H_
+
+void delay_ms(unsigned  long long ms);
+
+
+#endif /* DELAYMECH_H_ */
