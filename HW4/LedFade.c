7109123ba8da32ae25cbd3beb10496369aded0f8
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-02-13 15:52:49 +0000

    Update LedFade.c

--- HW4/LedFade.c
+++ HW4/LedFade.c
@@ -1,5 +1,6 @@
 
 
+
 #define F_CPU 16000000UL
 #include<avr/io.h>
 #include "DelayMech.h"
@@ -26,12 +27,28 @@ int main()
 		while(c<1000)
 		{
 			PORTB=0x20;
-			c+=10;
 			delay_us(c);
-			f-=10;
-			delay_us(f);
+
 			PORTB=0x00;
+			delay_us(f);
+			c+=10;
+			f-=10;
 		}
 	}
 	return 0;
 }
+
+//#define F_CPU 16000000UL
+//#include <avr/io.h>
+//#include <util/delay.h>
+
+//int main(void) {
+  //  DDRB |= 1<<PB5; /* set PB0 to output */
+//    while(1) {
+  //      PORTB &= ~(1<<PB5); /* LED on */
+       // _delay_ms(1);
+    //    PORTB |= 1<<PB5; /* LED off */
+     //   _delay_ms(1);
+    //}
+  //  return 0;
+//}
