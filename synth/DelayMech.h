41d9dfddeca3d6a32e2f25ded6f970d848f2843b
pall23-mech <142810678+pall23-mech@users.noreply.github.com>
2024-04-15 20:25:42 +0000

    Create DelayMech.h

---
+++ synth/DelayMech.h
@@ -0,0 +1,16 @@
+/*
+ * DelayMech.h
+ *
+ *  Created on: Feb 12, 2024
+ *      Author: palli
+ */
+
+#ifndef DELAYMECH_H_
+#define DELAYMECH_H_
+
+void delay_ms(unsigned long ms);
+void delay_us(unsigned long us);
+void delay_s(unsigned long s);
+
+
+#endif /* DELAYMECH_H_ */
